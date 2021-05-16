784e95d3f432f96da85550a14cb0e396ea4c6878
Zhihan Yang <yangz2@carleton.edu>
2021-05-16 18:47:59 +0800

    minor fix

--- results/plot_avg_return.py
+++ results/plot_avg_return.py
@@ -5,12 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--env', type=str, required=True)
-args = parser.parse_args()
-
-env_dir = args.env
-
 
 def ignore_hidden_and_png(items):
     """Sometimes we get random hidden folders..."""
@@ -26,31 +20,39 @@ def smooth(scalars, retain_prop=0.90):
     return smoothed_scalars
 
 
-for algo_folder in ignore_hidden_and_png(os.listdir(env_dir)):
+if __name__ == '__main__':
 
-    run_folders = ignore_hidden_and_png(os.listdir(os.path.join(env_dir, algo_folder)))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, required=True)
+    args = parser.parse_args()
 
-    ep_rets_s = []
+    env_dir = args.env
 
-    for run_folder in run_folders:
-        csv_path = os.path.join(env_dir, algo_folder, run_folder, 'progress.csv')
+    for algo_folder in ignore_hidden_and_png(os.listdir(env_dir)):
 
-        df = pd.read_csv(csv_path)
+        run_folders = ignore_hidden_and_png(os.listdir(os.path.join(env_dir, algo_folder)))
 
-        ep_rets = df['test_ep_ret'].to_numpy()
+        ep_rets_s = []
 
-        ep_rets_s.append(smooth(ep_rets))
+        for run_folder in run_folders:
+            csv_path = os.path.join(env_dir, algo_folder, run_folder, 'progress.csv')
 
-    ep_rets_s = np.array(ep_rets_s)
-    mean_ep_ret = ep_rets_s.mean(axis=0)  # average across all seeds
-    sd_ep_ret = ep_rets_s.std(axis=0)
+            df = pd.read_csv(csv_path)
 
-    plt.plot(np.arange(1, len(mean_ep_ret) + 1), mean_ep_ret, label=f'{algo_folder} ({len(run_folders)} runs)')
-    plt.fill_between(np.arange(1, len(mean_ep_ret) + 1), mean_ep_ret - sd_ep_ret, mean_ep_ret + sd_ep_ret, alpha=0.2)
+            ep_rets = df['test_ep_ret'].to_numpy()
 
-plt.title(args.env)
-plt.xlabel('Epoch')
-plt.ylabel('Test-time Return')
-plt.legend(loc='lower right')
-plt.grid()
-plt.savefig(f'{env_dir}/avg_return.png', dpi=200)
+            ep_rets_s.append(smooth(ep_rets))
+
+        ep_rets_s = np.array(ep_rets_s)
+        mean_ep_ret = ep_rets_s.mean(axis=0)  # average across all seeds
+        sd_ep_ret = ep_rets_s.std(axis=0)
+
+        plt.plot(np.arange(1, len(mean_ep_ret) + 1), mean_ep_ret, label=f'{algo_folder} ({len(run_folders)} runs)')
+        plt.fill_between(np.arange(1, len(mean_ep_ret) + 1), mean_ep_ret - sd_ep_ret, mean_ep_ret + sd_ep_ret, alpha=0.2)
+
+    plt.title(args.env)
+    plt.xlabel('Epoch')
+    plt.ylabel('Test-time Return')
+    plt.legend(loc='lower right')
+    plt.grid()
+    plt.savefig(f'{env_dir}/avg_return.png', dpi=200)
