7bc0bde4cd5992060eb46080b042487f85ffa42e
Zhihan Yang <yangz2@carleton.edu>
2021-05-19 23:05:55 +0800

    minor fix

--- results/plot.py
+++ results/plot.py
@@ -4,56 +4,48 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from plot_utils import neighbor_smooth
+from plot_utils import neighbor_smooth, ignore_hidden_and_png
 
 plt.rc('font', family='serif')
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', type=str, required=True)
+args = parser.parse_args()
 
-def ignore_hidden_and_png(items):
-    """Sometimes we get random hidden folders..."""
-    return [item for item in items if item[0] != '.' and not item.endswith('png')]
+env_dir = args.env
 
+for algo_folder in ignore_hidden_and_png(os.listdir(env_dir)):
 
-if __name__ == '__main__':
+    run_folders = ignore_hidden_and_png(os.listdir(os.path.join(env_dir, algo_folder)))
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, required=True)
-    args = parser.parse_args()
+    ep_rets_s = []
 
-    env_dir = args.env
+    for run_folder in run_folders:
+        csv_path = os.path.join(env_dir, algo_folder, run_folder, 'progress.csv')
 
-    for algo_folder in ignore_hidden_and_png(os.listdir(env_dir)):
+        df = pd.read_csv(csv_path)
 
-        run_folders = ignore_hidden_and_png(os.listdir(os.path.join(env_dir, algo_folder)))
+        steps = df['timestep'].to_numpy()
+        ep_rets = df['test_ep_ret'].to_numpy()
+        ep_rets = neighbor_smooth(list(ep_rets), 11)
 
-        ep_rets_s = []
+        ep_rets_s.append(ep_rets)
 
-        for run_folder in run_folders:
-            csv_path = os.path.join(env_dir, algo_folder, run_folder, 'progress.csv')
+    ep_rets_s = np.array(ep_rets_s)
+    mean_ep_ret = ep_rets_s.mean(axis=0)  # average across all seeds
+    std_ep_ret = ep_rets_s.std(axis=0)
 
-            df = pd.read_csv(csv_path)
+    plt.plot(steps, mean_ep_ret, label=f'{algo_folder} ({len(run_folders)} runs)')
+    plt.fill_between(steps, mean_ep_ret - std_ep_ret, mean_ep_ret + std_ep_ret, alpha=0.2)
 
-            steps = df['timestep'].to_numpy()
-            ep_rets = df['test_ep_ret'].to_numpy()
-            ep_rets = neighbor_smooth(list(ep_rets), 11)
+plt.title(args.env)
+plt.xlabel('Timestep')
+plt.ylabel('Test-time Return')
+plt.legend(loc='lower right')
 
-            ep_rets_s.append(ep_rets)
+plt.grid()
+plt.gca().spines['right'].set_visible(False)
+plt.gca().spines['top'].set_visible(False)
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-        ep_rets_s = np.array(ep_rets_s)
-        mean_ep_ret = ep_rets_s.mean(axis=0)  # average across all seeds
-        std_ep_ret = ep_rets_s.std(axis=0)
-
-        plt.plot(steps, mean_ep_ret, label=f'{algo_folder} ({len(run_folders)} runs)')
-        plt.fill_between(steps, mean_ep_ret - std_ep_ret, mean_ep_ret + std_ep_ret, alpha=0.2)
-
-    plt.title(args.env)
-    plt.xlabel('Timestep')
-    plt.ylabel('Test-time Return')
-    plt.legend(loc='lower right')
-
-    plt.grid()
-    plt.gca().spines['right'].set_visible(False)
-    plt.gca().spines['top'].set_visible(False)
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-
-    plt.savefig(f'{env_dir}/avg_return.png', dpi=200)
+plt.savefig(f'{env_dir}/avg_return.png', dpi=200)
--- results/plot_utils.py
+++ results/plot_utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 
+def ignore_hidden_and_png(items):
+    """Sometimes we get random hidden folders..."""
+    return [item for item in items if item[0] != '.' and not item.endswith('png')]
+
+
 def neighbor_smooth(scalars: list, num_neighbors) -> list:
     num_each_side = int(num_neighbors / 2 - 1)
     scalars = [scalars[0]] * num_each_side + scalars + [scalars[-1]] * num_each_side
