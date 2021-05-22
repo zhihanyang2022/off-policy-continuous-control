2acad107c3cdd6c596e36d228f1e1f057e779c97
Zhihan Yang <yangz2@carleton.edu>
2021-05-22 16:33:59 +0800

    minor fix

--- offpcc/basics/actors_and_critics_recurrent.py
+++ offpcc/basics/actors_and_critics_recurrent.py
@@ -22,11 +22,8 @@ class RecurrentGaussianActor(nn.Module):
         super().__init__()
 
         # self.pre_lstm = nn.Linear(in_features=input_dim, out_features=64)
-        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=64, batch_first=True)
-        # self.lstm.flatten_parameters()  # added this to resolve some arbitrary warning
-
-        self.mlp_layer_1 = nn.Linear(in_features=64, out_features=256)
-        self.mlp_layer_2 = nn.Linear(in_features=256, out_features=256)
+        self.layer1 = nn.LSTM(input_size=input_dim, hidden_size=256, batch_first=True)
+        self.layer2 = nn.Linear(in_features=256, out_features=256)
 
         self.means_layer = nn.Linear(in_features=256, out_features=action_dim)
         self.log_stds_layer = nn.Linear(in_features=256, out_features=action_dim)
@@ -37,10 +34,8 @@ class RecurrentGaussianActor(nn.Module):
     def forward(self, observations: torch.tensor) -> tuple:
 
         # x = F.relu(self.pre_lstm(observations))
-        x, _ = self.lstm(observations)
-
-        x = F.relu(self.mlp_layer_1(x))
-        x = F.relu(self.mlp_layer_2(x))
+        x, _ = self.layer1(observations)
+        x = F.relu(self.layer2(x))
 
         means, log_stds = self.means_layer(x), self.log_stds_layer(x)
         stds = torch.exp(torch.clamp(log_stds, self.LOG_STD_MIN, self.LOG_STD_MAX))
@@ -68,20 +63,15 @@ class RecurrentCritic(nn.Module):
         super().__init__()
 
         # self.pre_lstm = nn.Linear(in_features=input_dim, out_features=64)
-        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=64, batch_first=True)
-
-        self.mlp_layer_1 = nn.Linear(in_features=64+action_dim, out_features=256)
-        self.mlp_layer_2 = nn.Linear(in_features=256, out_features=256)
-
+        self.layer1 = nn.LSTM(input_size=input_dim, hidden_size=256, batch_first=True)
+        self.layer2 = nn.Linear(in_features=256+action_dim, out_features=256)
         self.q_values = nn.Linear(in_features=256, out_features=1)
 
     def forward(self, observations: torch.tensor, actions: torch.tensor):
 
         # x = F.relu(self.pre_lstm(observations))
-        x, _ = self.lstm(observations)  # mentally think about the output of lstm as "states"
-
-        x = F.relu(self.mlp_layer_1(torch.cat([x, actions], dim=2)))
-        x = F.relu(self.mlp_layer_2(x))
+        x, _ = self.layer1(observations)
+        x = F.relu(self.layer2(torch.cat([x, actions], dim=2)))
 
         q_values = self.q_values(x)
 
