48614c365aa2078f0597e8dacc4a4a2418ab4aa7
Zhihan Yang <yangz2@carleton.edu>
2021-05-29 21:34:48 +0800

    minor fix

---
+++ offpcc/basics/callbacks_sb3.py
@@ -0,0 +1,85 @@
+import gym
+from gym.wrappers import Monitor
+import warnings
+from typing import Any, Dict, Optional, Union
+
+from stable_baselines3.common.callbacks import EventCallback
+from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv, sync_envs_normalization
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class MyEvalCallback(EventCallback):
+
+    """
+    Minimal callback class for logging to wandb
+    Modified directly from
+    https://github.com/DLR-RM/stable-baselines3/blob/88e1be9ff5e04b7688efa44951f845b7daf5717f/stable_baselines3/common/callbacks.py#L268
+    """
+
+    def __init__(
+        self,
+        eval_env: Union[gym.Env, VecEnv],
+        seed: int = 0,
+        callback_on_new_best: Optional[BaseCallback] = None,
+        n_eval_episodes: int = 5,
+        eval_freq: int = 1000,
+        deterministic: bool = True,
+        render: bool = False,
+        verbose: int = 1,
+        warn: bool = True,
+    ):
+        super().__init__(callback_on_new_best, verbose=verbose)
+        self.n_eval_episodes = n_eval_episodes
+        self.eval_freq = eval_freq
+        self.deterministic = deterministic
+        self.render = render
+        self.warn = warn
+        self.seed = seed
+
+        # Convert to VecEnv for consistency
+        if not isinstance(eval_env, VecEnv):
+            eval_env = DummyVecEnv([lambda: eval_env])
+
+        if isinstance(eval_env, VecEnv):
+            assert eval_env.num_envs == 1, "You must pass only one environment for evaluation"
+
+        self.eval_env = eval_env
+
+    def _init_callback(self) -> None:
+        # Does not work in some corner cases, where the wrapper is not the same
+        if not isinstance(self.training_env, type(self.eval_env)):
+            warnings.warn("Training and eval env are not of the same type" f"{self.training_env} != {self.eval_env}")
+
+    def _on_step(self) -> bool:
+
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+
+            # Sync training and eval env if there is VecNormalize
+            sync_envs_normalization(self.training_env, self.eval_env)
+
+            episode_rewards, episode_lengths = evaluate_policy(
+                self.model,
+                self.eval_env,
+                n_eval_episodes=self.n_eval_episodes,
+                render=self.render,
+                deterministic=self.deterministic,
+                return_episode_rewards=True,
+            )
+
+            wandb.log({
+                'epoch': self.n_calls % self.eval_freq,
+                'timestep': self.n_calls,
+                'test_ep_ret': np.mean(episode_rewards),
+                'test_ep_len': np.mean(episode_lengths)
+            })
+
+        return True
+
+    def update_child_locals(self, locals_: Dict[str, Any]) -> None:
+        """
+        Update the references to the local variables.
+        :param locals_: the local variables during rollout collection
+        """
+        if self.callback:
+            self.callback.update_locals(locals_)
--- offpcc/basics/run_fns_sb3.py
+++ offpcc/basics/run_fns_sb3.py
@@ -1,94 +1,18 @@
+# @@@@@ imports @@@@@
+
 import gin
 import numpy as np
-import gym
-from gym.wrappers import Monitor
-import warnings
-from typing import Any, Dict, Optional, Union
+
 import wandb
 import os
 
 from stable_baselines3 import DDPG, TD3, SAC
 from stable_baselines3.common.noise import NormalActionNoise
-from stable_baselines3.common.callbacks import EventCallback
-from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv, sync_envs_normalization
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import BaseCallback
 
+from basics.callbacks_sb3 import MyEvalCallback
 
-class MyEvalCallback(EventCallback):
-
-    """
-    Minimal callback class for logging to wandb
-    Modified directly from
-    https://github.com/DLR-RM/stable-baselines3/blob/88e1be9ff5e04b7688efa44951f845b7daf5717f/stable_baselines3/common/callbacks.py#L268
-    """
-
-    def __init__(
-        self,
-        eval_env: Union[gym.Env, VecEnv],
-        seed: int = 0,
-        callback_on_new_best: Optional[BaseCallback] = None,
-        n_eval_episodes: int = 5,
-        eval_freq: int = 1000,
-        deterministic: bool = True,
-        render: bool = False,
-        verbose: int = 1,
-        warn: bool = True,
-    ):
-        super().__init__(callback_on_new_best, verbose=verbose)
-        self.n_eval_episodes = n_eval_episodes
-        self.eval_freq = eval_freq
-        self.deterministic = deterministic
-        self.render = render
-        self.warn = warn
-        self.seed = seed
-
-        # Convert to VecEnv for consistency
-        if not isinstance(eval_env, VecEnv):
-            eval_env = DummyVecEnv([lambda: eval_env])
-
-        if isinstance(eval_env, VecEnv):
-            assert eval_env.num_envs == 1, "You must pass only one environment for evaluation"
-
-        self.eval_env = eval_env
-
-    def _init_callback(self) -> None:
-        # Does not work in some corner cases, where the wrapper is not the same
-        if not isinstance(self.training_env, type(self.eval_env)):
-            warnings.warn("Training and eval env are not of the same type" f"{self.training_env} != {self.eval_env}")
-
-    def _on_step(self) -> bool:
-
-        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
-
-            # Sync training and eval env if there is VecNormalize
-            sync_envs_normalization(self.training_env, self.eval_env)
-
-            episode_rewards, episode_lengths = evaluate_policy(
-                self.model,
-                self.eval_env,
-                n_eval_episodes=self.n_eval_episodes,
-                render=self.render,
-                deterministic=self.deterministic,
-                return_episode_rewards=True,
-            )
-
-            wandb.log({
-                'epoch': self.n_calls % self.eval_freq,
-                'timestep': self.n_calls,
-                'test_ep_ret': np.mean(episode_rewards),
-                'test_ep_len': np.mean(episode_lengths)
-            })
-
-        return True
-
-    def update_child_locals(self, locals_: Dict[str, Any]) -> None:
-        """
-        Update the references to the local variables.
-        :param locals_: the local variables during rollout collection
-        """
-        if self.callback:
-            self.callback.update_locals(locals_)
+# @@@@@ wrapper functions for gin to configure models from SB3 @@@@@
 
 
 @gin.configurable(module=__name__)
@@ -205,6 +129,8 @@ def configure_sac(
     )
     return model
 
+# @@@@@ function for training and, once finished, saveing the model to wandb cloud @@@@@
+
 
 @gin.configurable(module=__name__)
 def train_and_save_model(
@@ -230,6 +156,7 @@ BASE_LOG_DIR = '../results_sb3'
 
 
 def make_log_dir(env_name, algo_name, seed) -> str:
+    """You should download the networks.zip file from wandb and put it inside this folder."""
     log_dir = f'{BASE_LOG_DIR}/{env_name}/{algo_name}/{seed}'
     return log_dir
 
@@ -248,4 +175,7 @@ def load_and_visualize_policy(
         deterministic=True,
         return_episode_rewards=True,
     )
-    print(episode_rewards, episode_lengths)
+    print(
+        'Rewards:', episode_rewards,
+        'Lengths:', episode_lengths
+    )
