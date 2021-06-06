1ea678fe028d7337b6544900adc4fff367a6966f
Zhihan Yang <yangz2@carleton.edu>
2021-06-06 16:44:06 +0800

    minor fix

--- offpcc/algorithms_recurrent/recurrent_sac.py
+++ offpcc/algorithms_recurrent/recurrent_sac.py
@@ -140,13 +140,15 @@ class RecurrentSAC(RecurrentOffPolicyRLAlgorithm):
         Q1_summary_1_T, Q1_summary_2_Tplus1 = Q1_summary[:, :-1, :], Q1_summary_targ[:, 1:, :]
         Q2_summary_1_T, Q2_summary_2_Tplus1 = Q2_summary[:, :-1, :], Q2_summary_targ[:, 1:, :]
 
+        assert actor_summary.shape == (bs, num_bptt, self.hidden_dim)
+
         # compute predictions
 
         Q1_predictions = self.Q1(Q1_summary_1_T, b.a)
         Q2_predictions = self.Q2(Q2_summary_1_T, b.a)
 
-        assert Q1_predictions.shape == (bs, 1)
-        assert Q2_predictions.shape == (bs, 1)
+        assert Q1_predictions.shape == (bs, num_bptt, 1)
+        assert Q2_predictions.shape == (bs, num_bptt, 1)
 
         # compute targets
 
