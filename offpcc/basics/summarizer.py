54468edbb9bd9fd8c2bd1f1e6151822c71054427
Zhihan Yang <yangz2@carleton.edu>
2021-06-16 10:19:25 +0800

    minor fix

--- offpcc/basics/summarizer.py
+++ offpcc/basics/summarizer.py
@@ -8,7 +8,7 @@ class Summarizer(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, num_rnn_layers=2):
         super().__init__()
-        self.rnn = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=num_rnn_layers)
+        self.rnn = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=num_rnn_layers, dropout=0.2)
 
     def forward(self, observations, hidden=None, return_hidden=False):
         self.rnn.flatten_parameters()
