8a4a96cf3196e2a50b76294127ea399116fbe456
Zhihan Yang <yangz2@carleton.edu>
2021-06-19 20:38:57 +0800

    minor fix

--- offpcc/basics/summarizer.py
+++ offpcc/basics/summarizer.py
@@ -7,9 +7,14 @@ import torch.nn as nn
 class Summarizer(nn.Module):
 
     def __init__(self, input_dim, hidden_dim, num_rnn_layers=2):
+
         super().__init__()
+
         self.rnn = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=num_rnn_layers)
         self.layer_norm = nn.LayerNorm(normalized_shape=hidden_dim)
+
+        # normalized_shape=hidden_dim means that we only normalized the hidden dimensions, individually
+        # https://pytorch.org/docs/stable/generated/torch.nn.LayerNorm.html#torch.nn.LayerNorm
 
     def forward(self, observations, hidden=None, return_hidden=False):
         self.rnn.flatten_parameters()
