0db4da5f5d898bbb5abd6fdebd4d43d4c4311327
Zhihan Yang <yangz2@carleton.edu>
2021-06-20 10:43:30 +0800

    minor fix

--- offpcc/basics/action_noise_scheduler.py
+++ offpcc/basics/action_noise_scheduler.py
@@ -9,17 +9,17 @@ class ActionNoiseScheduler:
         self,
         init_action_noise: float,
         schedule: Callable,
-        fina_action_noise: float = 0.05
+        min_action_noise: float = 0.05
     ):
 
         self.init_action_noise = init_action_noise
         self.schedule = schedule  # a function of num_updates
-        self.fina_action_noise = fina_action_noise
+        self.min_action_noise = min_action_noise
 
         self.for_which_update = 1
 
     def get_new_action_noise(self) -> float:
         new_action_noise = float(np.clip(self.init_action_noise * self.schedule(self.for_which_update),
-                                         a_min=self.fina_action_noise, a_max=None))
+                                         a_min=self.min_action_noise, a_max=None))
         self.for_which_update += 1
         return new_action_noise
