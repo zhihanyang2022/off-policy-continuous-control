842db6e418b2efec227321c367330c6497d282d2
Zhihan Yang <yangz2@carleton.edu>
2021-06-22 22:30:51 +0800

    minor fix

--- offpcc/test_env_bumps_normal.py
+++ offpcc/test_env_bumps_normal.py
@@ -7,7 +7,8 @@ parser.add_argument('--env', required=True, type=str)
 parser.add_argument('--use_random_policy', action="store_true")
 args = parser.parse_args()
 
-env = gym.wrappers.RescaleAction(gym.make(args.env), -1, 1)
+env = gym.wrappers.RescaleAction(gym.make(args.env, rendering=True), -1, 1)
+env2 = gym.wrappers.RescaleAction(gym.make(args.env), -1, 1)
 
 print('=> Env:', env)
 print('=> Timeout:', env.spec.max_episode_steps)
@@ -17,14 +18,17 @@ print('=> Observation space high:', env.observation_space.high)
 print('=> Random trajectory:')
 
 state = env.reset()
+state2 = env2.reset()
 print(state)
 
 ret = 0
 cnt = 0
 while True:
     state, reward, done, info = env.step(env.action_space.sample())
+    _ = env2.step(env2.action_space.sample())
     ret += reward
     cnt += 1
+    env.render()
     print(state, reward, done)
     if done:
         break
