ff749f83eb04c236c61a12837040f2557e901017
Zhihan Yang <yangz2@carleton.edu>
2021-06-22 22:27:52 +0800

    change test_env to reproduce 4 bumps scenario

--- offpcc/test_env.py
+++ offpcc/test_env.py
@@ -7,8 +7,8 @@ parser.add_argument('--env', required=True, type=str)
 parser.add_argument('--use_random_policy', action="store_true")
 args = parser.parse_args()
 
-env = gym.wrappers.RescaleAction(gym.make(args.env), -1, 1)
-env2 = gym.wrappers.RescaleAction(gym.make(args.env, rendering=True), -1, 1)
+env = gym.wrappers.RescaleAction(gym.make(args.env, rendering=True), -1, 1)
+env2 = gym.wrappers.RescaleAction(gym.make(args.env), -1, 1)
 
 print('=> Env:', env)
 print('=> Timeout:', env.spec.max_episode_steps)
