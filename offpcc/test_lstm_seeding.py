64fca2d931268522c44854ad6cf0cd81601618d9
Zhihan Yang <yangz2@carleton.edu>
2021-06-28 15:10:14 +0800

    added test_lstm_seeding.py

---
+++ offpcc/test_lstm_seeding.py
@@ -0,0 +1,25 @@
+import torch
+
+input = torch.randn(32, 100, 8)
+target = torch.randn(32, 100, 8)
+
+
+def trial():
+    torch.manual_seed(42)
+    layer = torch.nn.LSTM(8, 256, batch_first=True)
+    output, _ = layer(input)
+    loss = torch.mean((output - target) ** 2)
+    loss.backward()
+    return layer, output
+
+
+trial1_layer, trial1_output = trial()
+trial2_layer, trial2_output = trial()
+
+gradient_identical = True
+for p1, p2 in zip(trial1_layer.parameters(), trial2_layer.parameters()):
+    if not torch.eq(p1, p2):
+        gradient_identical = False
+
+print('Output identical?', torch.eq(trial1_output, trial2_output))
+print('Gradient identical', gradient_identical)
