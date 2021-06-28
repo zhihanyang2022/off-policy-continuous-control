dd3ae31b34ecb31a5dee317271494c71aa83e411
Zhihan Yang <yangz2@carleton.edu>
2021-06-28 15:18:20 +0800

    minor fix

--- offpcc/test_lstm_seeding.py
+++ offpcc/test_lstm_seeding.py
@@ -16,10 +16,17 @@ def trial():
 trial1_layer, trial1_output = trial()
 trial2_layer, trial2_output = trial()
 
+parameter_identical = True
 gradient_identical = True
+
 for p1, p2 in zip(trial1_layer.parameters(), trial2_layer.parameters()):
+
+    if not torch.all(torch.eq(p1, p2)):
+        parameter_identical = False
+
     if not torch.all(torch.eq(p1.grad, p2.grad)):
         gradient_identical = False
 
 print('Output identical?', torch.all(torch.eq(trial1_output, trial2_output)))
+print('Parameter identical', parameter_identical)
 print('Gradient identical', gradient_identical)
