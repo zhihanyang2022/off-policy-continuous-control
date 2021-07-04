2bec576baf00f28be539d71b63f18ed36cdd4eaf
Zhihan Yang <yangz2@carleton.edu>
2021-07-04 11:17:09 +0800

    minor fix

--- offpcc/domains/__init__.py
+++ offpcc/domains/__init__.py
@@ -1,47 +1,5 @@
 from gym.envs.registration import register
 
-# Caution: all envs here must set max_episode_steps!!
-
-register(
-    id='cartpole-continuous-v0',
-    entry_point='domains.cartpole_continuous:ContinuousCartPoleEnv',
-    max_episode_steps=200
-)
-
-register(
-    id='cartpole-continuous-long-v0',
-    entry_point='domains.cartpole_continuous:ContinuousCartPoleEnv',
-    max_episode_steps=1000
-)
-
-# partially observable: position only
-
-register(
-    id='cartpole-continuous-long-p-v0',
-    entry_point='domains.cartpole_continuous:continuous_cartpole_position_env',
-    max_episode_steps=1000
-)
-
-register(
-    id='cartpole-continuous-long-p-concat-v0',  # concat obs to approximate MDP
-    entry_point='domains.cartpole_continuous:continuous_cartpole_position_concat_env',
-    max_episode_steps=1000
-)
-
-# partially observable: velocity only
-
-register(
-    id='cartpole-continuous-long-v-v0',
-    entry_point='domains.cartpole_continuous:continuous_cartpole_velocity_env',
-    max_episode_steps=1000
-)
-
-register(
-    id='cartpole-continuous-long-v-concat-v0',  # concat obs to approximate MDP
-    entry_point='domains.cartpole_continuous:continuous_cartpole_velocity_concat_env',
-    max_episode_steps=1000
-)
-
 # car heaven hell
 
 register(
@@ -277,4 +235,26 @@ register(
     id='dmc-walker-walk-mdp-img-concat3-v0',
     entry_point='domains.dmc_walker_walk:mdp_img_concat3',
     max_episode_steps=500
+)
+
+# ============================================================================================
+# for comparison with VRM
+# ============================================================================================
+
+register(
+    id='cartpole-continuous-long-v0',
+    entry_point='domains.cartpole_balance:mdp',
+    max_episode_steps=1000
+)
+
+register(
+    id='cartpole-continuous-long-pomdp-v0',
+    entry_point='domains.cartpole_balance:pomdp',
+    max_episode_steps=1000
+)
+
+register(
+    id='cartpole-continuous-long-mdp-concat5-v0',
+    entry_point='domains.cartpole_balance:mdp_concat5',
+    max_episode_steps=1000
 )
