39f915dab0adee9b3f87cace0da119a8683481b9
hhn1n15 <hainhbkhn@gmail.com>
2021-08-03 18:58:16 -0400

    minor

--- offpcc/launch.py
+++ offpcc/launch.py
@@ -88,7 +88,7 @@ for run_id in args.run_id:  # args.run_id is a list of ints; could contain more 
     else:
 
         run = wandb.init(
-            project="car-flag-baselines",
+            project="box-baselines",
             # entity='hainh22',
             group=f"{args.algo} {args.env}",
             settings=wandb.Settings(_disable_stats=True),
