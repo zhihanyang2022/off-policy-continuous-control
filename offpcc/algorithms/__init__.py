c3a43917cdbf4bc22e4c1f8cb6bf7df9e113309e
hhn1n15 <hainhbkhn@gmail.com>
2021-08-03 18:44:05 -0400

    update

--- offpcc/algorithms/__init__.py
+++ offpcc/algorithms/__init__.py
@@ -1,4 +1,5 @@
 from algorithms.ddpg import DDPG
 from algorithms.td3 import TD3
 from algorithms.sac import SAC
-from algorithms.convolutional_sac import ConvolutionalSAC
+from algorithms.ddpg_lstm import RDPG
+from algorithms.sac_lstm import SAC_LSTM
