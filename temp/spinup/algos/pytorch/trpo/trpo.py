c8247f1f80c8b46450207712199b2d16637eb4c4
Zhihan Yang <yangz2@carleton.edu>
2021-09-01 11:21:45 +0800

    minor fix

--- temp/spinup/algos/pytorch/trpo/trpo.py
+++
@@ -1,4 +0,0 @@
-def trpo(*args, **kwargs):
-    print('\n\nUnfortunately, TRPO has not yet been implemented in PyTorch '\
-        + 'for Spinning Up. TRPO will migrate some time in the future.\n\n')
-    raise NotImplementedError
