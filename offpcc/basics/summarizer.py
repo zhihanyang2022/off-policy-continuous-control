6eb35e2d15c239732dc1819a3d1ffb2f63f7f3af
Zhihan Yang <yangz2@carleton.edu>
2021-09-26 10:18:54 +0800

    minor fix

--- offpcc/basics/summarizer.py
+++ offpcc/basics/summarizer.py
@@ -6,15 +6,22 @@ import torch.nn as nn
 @gin.configurable(module=__name__)
 class Summarizer(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, num_rnn_layers=2):
+    def __init__(self, input_dim, hidden_dim, num_layers=2, recurrent_type='lstm'):
 
         super().__init__()
 
-        self.rnn = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=num_rnn_layers)
+        if recurrent_type == 'lstm':
+            self.recurrent_module = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=num_layers)
+        elif recurrent_type == 'rnn':
+            self.recurrent_module = nn.RNN(input_dim, hidden_dim, batch_first=True, num_layers=num_layers)
+        elif recurrent_type == 'gru':
+            self.recurrent_module = nn.GRU(input_dim, hidden_dim, batch_first=True, num_layers=num_layers)
+        else:
+            raise ValueError(f"{recurrent_type} not recognized")
 
     def forward(self, observations, hidden=None, return_hidden=False):
-        self.rnn.flatten_parameters()
-        summary, hidden = self.rnn(observations, hidden)
+        self.recurrent_module.flatten_parameters()
+        summary, hidden = self.recurrent_module(observations, hidden)
         if return_hidden:
             return summary, hidden
         else:
