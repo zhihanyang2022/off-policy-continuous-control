a6fe4ad85bf26493cfa357acdfefa24c06e111b9
Zhihan Yang <yangz2@carleton.edu>
2021-12-17 17:56:59 +0800

    minor fix

--- offpcc/basics/summarizer.py
+++ offpcc/basics/summarizer.py
@@ -2,7 +2,7 @@ import gin
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+# import torch.nn.functional as F
 from basics.utils import get_device
 
 
@@ -34,97 +34,97 @@ class Summarizer(nn.Module):
 # from https://pytorch.org/tutorials/beginner/translation_transformer.html
 
 
-class TransformerEncoderLayer(nn.Module):
-    r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
-    This standard encoder layer is based on the paper "Attention Is All You Need".
-    Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez,
-    Lukasz Kaiser, and Illia Polosukhin. 2017. Attention is all you need. In Advances in
-    Neural Information Processing Systems, pages 6000-6010. Users may modify or implement
-    in a different way during application.
-
-    Args:
-        d_model: the number of expected features in the input (required).
-        nhead: the number of heads in the multiheadattention models (required).
-        dim_feedforward: the dimension of the feedforward network model (default=2048).
-        dropout: the dropout value (default=0.1).
-        activation: the activation function of the intermediate layer, can be a string
-            ("relu" or "gelu") or a unary callable. Default: relu
-        layer_norm_eps: the eps value in layer normalization components (default=1e-5).
-        batch_first: If ``True``, then the input and output tensors are provided
-            as (batch, seq, feature). Default: ``False``.
-        norm_first: if ``True``, layer norm is done prior to attention and feedforward
-            operations, respectivaly. Otherwise it's done after. Default: ``False`` (after).
-
-    Examples::
-        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
-        >>> src = torch.rand(10, 32, 512)
-        >>> out = encoder_layer(src)
-
-    Alternatively, when ``batch_first`` is ``True``:
-        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8, batch_first=True)
-        >>> src = torch.rand(32, 10, 512)
-        >>> out = encoder_layer(src)
-    """
-    __constants__ = ['batch_first', 'norm_first']
-
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation=F.relu,
-                 layer_norm_eps=1e-5, batch_first=False, norm_first=False,
-                 device=None, dtype=None) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(TransformerEncoderLayer, self).__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=batch_first,
-                                            **factory_kwargs)
-        # Implementation of Feedforward model
-        self.linear1 = nn.Linear(d_model, dim_feedforward, **factory_kwargs)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model, **factory_kwargs)
-
-        # self.norm_first = norm_first
-        # self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
-        # self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-
-        # Legacy string support for activation function.
-        self.activation = activation
-
-    def __setstate__(self, state):
-        if 'activation' not in state:
-            state['activation'] = F.relu
-        super(TransformerEncoderLayer, self).__setstate__(state)
-
-    def forward(self, src, src_mask, src_key_padding_mask=None):
-        r"""Pass the input through the encoder layer.
-
-        Args:
-            src: the sequence to the encoder layer (required).
-            src_mask: the mask for the src sequence (optional).
-            src_key_padding_mask: the mask for the src keys per batch (optional).
-
-        Shape:
-            see the docs in Transformer class.
-        """
-
-        # see Fig. 1 of https://arxiv.org/pdf/2002.04745v1.pdf
-
-        x = src
-        x = x + self._sa_block(x, src_mask, src_key_padding_mask)
-        x = x + self._ff_block(x)
-
-        return x
-
-        # self-attention block
-    def _sa_block(self, x, attn_mask, key_padding_mask):
-        x = self.self_attn(x, x, x,
-                           attn_mask=attn_mask,
-                           key_padding_mask=key_padding_mask,
-                           need_weights=False)[0]
-        return self.dropout1(x)
-
-    # feed forward block
-    def _ff_block(self, x):
-        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
-        return self.dropout2(x)
+# class TransformerEncoderLayer(nn.Module):
+#     r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
+#     This standard encoder layer is based on the paper "Attention Is All You Need".
+#     Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez,
+#     Lukasz Kaiser, and Illia Polosukhin. 2017. Attention is all you need. In Advances in
+#     Neural Information Processing Systems, pages 6000-6010. Users may modify or implement
+#     in a different way during application.
+#
+#     Args:
+#         d_model: the number of expected features in the input (required).
+#         nhead: the number of heads in the multiheadattention models (required).
+#         dim_feedforward: the dimension of the feedforward network model (default=2048).
+#         dropout: the dropout value (default=0.1).
+#         activation: the activation function of the intermediate layer, can be a string
+#             ("relu" or "gelu") or a unary callable. Default: relu
+#         layer_norm_eps: the eps value in layer normalization components (default=1e-5).
+#         batch_first: If ``True``, then the input and output tensors are provided
+#             as (batch, seq, feature). Default: ``False``.
+#         norm_first: if ``True``, layer norm is done prior to attention and feedforward
+#             operations, respectivaly. Otherwise it's done after. Default: ``False`` (after).
+#
+#     Examples::
+#         >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+#         >>> src = torch.rand(10, 32, 512)
+#         >>> out = encoder_layer(src)
+#
+#     Alternatively, when ``batch_first`` is ``True``:
+#         >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8, batch_first=True)
+#         >>> src = torch.rand(32, 10, 512)
+#         >>> out = encoder_layer(src)
+#     """
+#     __constants__ = ['batch_first', 'norm_first']
+#
+#     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation=F.relu,
+#                  layer_norm_eps=1e-5, batch_first=False, norm_first=False,
+#                  device=None, dtype=None) -> None:
+#         factory_kwargs = {'device': device, 'dtype': dtype}
+#         super(TransformerEncoderLayer, self).__init__()
+#         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=batch_first,
+#                                             **factory_kwargs)
+#         # Implementation of Feedforward model
+#         self.linear1 = nn.Linear(d_model, dim_feedforward, **factory_kwargs)
+#         self.dropout = nn.Dropout(dropout)
+#         self.linear2 = nn.Linear(dim_feedforward, d_model, **factory_kwargs)
+#
+#         # self.norm_first = norm_first
+#         # self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+#         # self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+#         self.dropout1 = nn.Dropout(dropout)
+#         self.dropout2 = nn.Dropout(dropout)
+#
+#         # Legacy string support for activation function.
+#         self.activation = activation
+#
+#     def __setstate__(self, state):
+#         if 'activation' not in state:
+#             state['activation'] = F.relu
+#         super(TransformerEncoderLayer, self).__setstate__(state)
+#
+#     def forward(self, src, src_mask, src_key_padding_mask=None):
+#         r"""Pass the input through the encoder layer.
+#
+#         Args:
+#             src: the sequence to the encoder layer (required).
+#             src_mask: the mask for the src sequence (optional).
+#             src_key_padding_mask: the mask for the src keys per batch (optional).
+#
+#         Shape:
+#             see the docs in Transformer class.
+#         """
+#
+#         # see Fig. 1 of https://arxiv.org/pdf/2002.04745v1.pdf
+#
+#         x = src
+#         x = x + self._sa_block(x, src_mask, src_key_padding_mask)
+#         x = x + self._ff_block(x)
+#
+#         return x
+#
+#         # self-attention block
+#     def _sa_block(self, x, attn_mask, key_padding_mask):
+#         x = self.self_attn(x, x, x,
+#                            attn_mask=attn_mask,
+#                            key_padding_mask=key_padding_mask,
+#                            need_weights=False)[0]
+#         return self.dropout1(x)
+#
+#     # feed forward block
+#     def _ff_block(self, x):
+#         x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+#         return self.dropout2(x)
 
 
 class PositionalEncoding(nn.Module):
@@ -163,7 +163,7 @@ class TransformerSummarizer(nn.Module):
 
         self.input_projector = nn.Linear(obs_size, hidden_dim)
         self.positional_encoding = PositionalEncoding(hidden_dim, max_len)
-        encoder_layer = TransformerEncoderLayer(
+        encoder_layer = nn.TransformerEncoderLayer(
             d_model=256, nhead=8,
             dim_feedforward=512, dropout=0,
             batch_first=True
